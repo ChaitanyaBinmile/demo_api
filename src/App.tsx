42214636ea91e7b87aa34cc0b1d9d689547882dc
Chaitanya Varshney <chaitanya.v@binmile.com>
2023-09-15 12:30:06 +0530

    css

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,12 @@
 import './App.css'
+import { Button } from './components/button/Button'
+import { Result } from './components/result/Result'
 
 function App() {
   return (
-    <div>
-      
+    <div className='bg-[#20272c] h-screen w-screen flex flex-col justify-center items-center gap-8'>
+      <Button/>
+      <Result/>
     </div>
   )
 }
---
+++ src/components/result/Result.tsx
@@ -0,0 +1,8 @@
+export const Result = () => {
+  return (
+    <div className="flex flex-row flex-wrap  w-screen gap-4 justify-center">
+      <div className="h-[150px] w-[220px] bg-slate-600 border-[2px] border-solid "></div>
+        
+    </div>
+  )
+}
